e3e9fec9475822fcf501741634adcd2ee8a5be6f
Constance Amatey <consamate57@gmail.com>
2023-11-01 20:21:03 +0000

    insert right child

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - node to be inserted
+ * @parent: Pointer to the parent node
+ * @value: Value of the parent node
+ * Return: new node if successful
+ **/
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new_node = binary_tree_node(parent, value);
+	if (!new_node)
+		return (NULL);
+
+	if (parent->right == NULL)
+		parent->right = new_node;
+	else
+	{
+		new_node->right = parent->right;
+		parent->right = new_node;
+		new_node->right->parent = new_node;
+	}
+	return (new_node);
+}
