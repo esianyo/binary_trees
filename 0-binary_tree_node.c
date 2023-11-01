d7397a5e5872e86b56b14fbf216bb95fd9b2dcac
Constance Amatey <consamate57@gmail.com>
2023-11-01 19:21:01 +0000

    create tree node without children

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - binary tree node
+ * @parent: Pointer to the parent node
+ * @value: Value of the parent node
+ * Return: new node if successful
+ **/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = value;
+	new_node->parent = parent;
+	return (new_node);
+}
