18378f05db1f64324747d356ee7d993fec0a5d26
esianyo <esianicd@gmail.com>
2023-11-01 22:15:18 +0000

    task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -11,6 +11,6 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if ((tree == NULL) || (func == NULL))
 		return;
 	binary_tree_preorder(tree->left, func);
-	func(tree->n)
+	func(tree->n);
 	binary_tree_preorder(tree->right, func);
 }
