e6fab9eea446b13845b184f507af01c8b8904354
Constance Amatey <consamate57@gmail.com>
2023-11-02 01:29:34 +0000

    calculate height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,41 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_height - gets the height of the tree
+* @tree: tree whose height is being calculated
+* Return: height of tree
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t height, lheight, rheight;
+
+	height = 0;
+	right_height = 0;
+	left_height = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left != NULL)
+	{
+		left_height = binary_tree_height(tree->left);
+		left_height += 1;
+	}
+	else
+		left_height = 0;
+
+	if (tree->right != NULL)
+	{
+		right_height = binary_tree_height(tree->right);
+		right_height += 1;
+	}
+	else
+		right_height = 0;
+
+	if (left_height >= right_height)
+		height = (left_height);
+	else 
+		height = (right_height);
+
+	return (height);
+}
