f4777a8b1ca4f0b9adfddd23f78a2adec648589f
Constance Amatey <consamate57@gmail.com>
2023-11-02 03:53:24 +0000

    calculate depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - determine a node's depth
+ * @node: Node whose depth is being calculated
+ * Return: The depth of the node
+ */
+size_t binary_tree_depth(const binary_tree_t *node)
+{
+	size_t depth;
+
+	depth = 0;
+	if (!node)
+		return (depth);
+
+	while (node->parent != NULL)
+		node = node->parent;
+		depth++;
+	return (depth);
+}
